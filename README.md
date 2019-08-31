12397d9dbb3869d656e16014ad749792546f0c07
Adrian Curless <awcurless@wpi.edu>
2019-08-31 00:00:23 -0400

    Add a signal handler to prevent termination with control-c

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # rsh
 
 A simple linux command shell written in rust for educational purposes.
+
+Supports:
+* Environment variables via `export`
+* Piping via `|`
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,17 @@ use std::string::String;
 
 use crate::env::env as rshell_env;
 use crate::rsh::rsh as rshell;
+
+use simple_signal::Signal;
+
 /**
  * Entry point. Continuously process lines of input.
  */
 fn main() {
+    // Intercept SIG_INT and SIG_TERM to prevent terminating the shell accidentally.
+    // Type 'exit' to terminate.
+    simple_signal::set_handler(&[Signal::Int, Signal::Term], |_signals| ());
+
     let mut prompt = String::from("> ");
     let mut environ: HashMap<String, String> = HashMap::new();
 
